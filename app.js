ca2b0d2f1e2865de7399bbda5ab8e0d11f6a19ef
ajmalMundumuzhi <muhajmal73@gmail.com>
2025-09-10 12:37:40 +0530

    access origin for thelimelightonline.in

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5000",
   "https://webzine.onrender.com",
+  "https://www.thelimelightonline.in",
 ];
 
 const corsOptions = {
