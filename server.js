2ef1b4077556a643a3e61f104491a4daef5242ee
ajmalMundumuzhi <muhajmal73@gmail.com>
2025-12-23 14:58:21 +0530

    refactor: changed express.json to another

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 const connectDB = require('./config/dataBase');
@@ -42,6 +41,8 @@ app.use('/api/categories', categoryRoutes);
 app.use('/api/authors', authorRoutes);
 
 app.get('/api/health', (req, res) => res.send('API is running...'));
+
+app.use(express.json());
 
 connectDB();
 app.get('/', (req,res) => {
